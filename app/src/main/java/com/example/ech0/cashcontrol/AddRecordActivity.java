c7b758823f7475fb5c54bd27aa9a474c85a05283
Kovalchuk Ilya <ilyakovey@gmail.com>
2017-04-20 09:23:02 -0700

    Форма добавления записи

---
+++ app/src/main/java/com/example/ech0/cashcontrol/AddRecordActivity.java
@@ -0,0 +1,22 @@
+package com.example.ech0.cashcontrol;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class AddRecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_record);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.categories_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+}
--- app/src/main/java/com/example/ech0/cashcontrol/MainActivity.java
+++ app/src/main/java/com/example/ech0/cashcontrol/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.ech0.cashcontrol.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +15,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+    public void addRecord(View view) {
+        Intent intent = new Intent(this, AddRecordActivity.class);
         startActivity(intent);
     }
 }
