d2c05e5b0f729b46f3229243c99077867dae99b5
Ilya Kovalchuk <ilyakovey@gmail.com>
2017-04-26 19:01:37 +0500

    Вывод и добавление записей 1

--- app/src/main/java/com/example/ech0/cashcontrol/MainActivity.java
+++ app/src/main/java/com/example/ech0/cashcontrol/MainActivity.java
@@ -5,42 +5,37 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<Record> recordsList = new ArrayList<>();
         DatabaseHandler DbHandler = new DatabaseHandler(this);
         SQLiteDatabase db = DbHandler.getReadableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM " + DatabaseContract.RecordsTable.TABLE_NAME, null);
+        cursor = db.rawQuery("SELECT * FROM " + DatabaseContract.RecordsTable.TABLE_NAME, null);
 
-        while (cursor.moveToNext()) {
-            Record record = new Record();
-
-            record.set_id(cursor.getInt(0));
-            record.set_date(cursor.getInt(1));
-            record.set_sum(cursor.getInt(2));
-            record.set_category(cursor.getInt(3));
-            record.set_comment(cursor.getString(4));
-
-            recordsList.add(record);
-        }
-        cursor.close();
-
+        ListView lvRecords = (ListView) findViewById(R.id.lvRecords);
+        TableCursorAdapter tableAdapter = new TableCursorAdapter(this, cursor, 0);
+        lvRecords.setAdapter(tableAdapter);
     }
 
     public void addRecordForm(View view) {
         Intent intent = new Intent(this, AddRecordActivity.class);
         startActivity(intent);
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        cursor.close();
     }
 }
