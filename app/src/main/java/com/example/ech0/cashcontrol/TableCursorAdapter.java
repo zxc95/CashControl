81bba53ea07431ec3fc0fb50934c4f5e0c2cd9d2
Ilya Kovalchuk <ilyakovey@gmail.com>
2017-04-26 19:06:57 +0500

    Вывод и добавление записей 1

---
+++ app/src/main/java/com/example/ech0/cashcontrol/TableCursorAdapter.java
@@ -0,0 +1,40 @@
+package com.example.ech0.cashcontrol;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+
+public class TableCursorAdapter extends CursorAdapter {
+    private LayoutInflater cursorInflater;
+
+    public TableCursorAdapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+        cursorInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return cursorInflater.inflate(R.layout.item_record, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        TextView tvSum = (TextView) view.findViewById(R.id.tvSum);
+        TextView tvCat = (TextView) view.findViewById(R.id.tvCat);
+        TextView tvDate =(TextView) view.findViewById(R.id.tvDate);
+        TextView tvCom = (TextView) view.findViewById(R.id.tvCom);
+        int sum = cursor.getInt(cursor.getColumnIndexOrThrow(DatabaseContract.RecordsTable.COLUMN_NAME_SUM));
+        int cat = cursor.getInt(cursor.getColumnIndexOrThrow(DatabaseContract.RecordsTable.COLUMN_NAME_CATEGORY));
+        int date = cursor.getInt(cursor.getColumnIndexOrThrow(DatabaseContract.RecordsTable.COLUMN_NAME_DATE));
+        String com = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseContract.RecordsTable.COLUMN_NAME_COMMENT));
+        tvSum.setText(String.valueOf(sum));
+        tvCat.setText(String.valueOf(cat));
+        tvDate.setText(String.valueOf(date));
+        tvCom.setText(com);
+    }
+}
